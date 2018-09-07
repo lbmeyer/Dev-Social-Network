759420edada3997421ba17f58e2ae9a5fb950413
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2018-09-07 16:51:06 -0400

    Frontend project files created and setup with Bootstrap

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(newUser)
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form action="dashboard.html">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
