4436065e61cef03f43faddc1df87f2ada584398e
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2018-09-09 17:30:31 -0400

    Added Profile Reducer and Get current profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
-
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -92,5 +93,5 @@ const mapStateToProps = state => ({
 });
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
